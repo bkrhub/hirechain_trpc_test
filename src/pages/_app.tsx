3cd629222d3bf232646822e664b9e2841755e780
Chris Baker <bkrhub@gmail.com>
2023-07-27 11:53:30 +0200

    add react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
